7e52ddcc25b8d232c88312d89911cbc8e491d688
Mike <245413040@qq.com>
2023-10-25 18:17:59 +0800

    flutter_learn_8

--- flutter_learn_layout_8/lib/main.dart
+++ flutter_learn_layout_8/lib/main.dart
@@ -9,22 +9,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Flutter layout demo'),
-        ),
-        body: Column(
-          children: [
-            titleSection,
-          ],
-        ),
-      ),
-    );
-  }
 
-  Widget titleSection = Container(
+    Widget titleSection = Container(
     padding: const EdgeInsets.all(32),
     child: Row(
       children: [
@@ -58,4 +44,72 @@ class MyApp extends StatelessWidget {
       ],
     ),
   );
+
+    Color color = Theme.of(context).primaryColor;
+  
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+       _buildButtonColumn(color, Icons.call, "CALL"),
+       _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+       _buildButtonColumn(color, Icons.share, 'SHARE')
+      ],
+    );
+
+    Widget textSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: const Text(
+          'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+          'Alps. Situated 1,578 meters above sea level, it is one of the '
+          'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+          'half-hour walk through pastures and pine forest, leads you to the '
+          'lake, which warms to 20 degrees Celsius in the summer. Activities '
+          'enjoyed here include rowing, and riding the summer toboggan run.',
+          softWrap: true,
+        ),
+    );
+
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter layout demo'),
+        ),
+        body: ListView(
+          children: [
+            Image.asset(
+              'images/lake.jpg', 
+              width: 600, 
+              height: 240, 
+              fit: BoxFit.cover
+            ),
+            titleSection,
+            buttonSection,
+            textSection
+          ],
+        ),
+      ),
+    );
+  }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label){
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          padding: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color
+            ),
+          ),
+        )
+      ],
+    );
+  }
 }
